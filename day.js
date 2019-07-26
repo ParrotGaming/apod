5f39920ecb4668713d3234631bf929c537e79dd7
Krazypants207 <killian@onyxwear.com>
2019-07-25 18:01:37 -0700

    added resolution switcher

---
+++ day.js
@@ -0,0 +1,52 @@
+class Day {
+    constructor(data) {
+        this.data = data
+    }
+
+    createView(container, res_btns) {
+
+    let card = document.createElement('div');
+
+    let title_el = document.createElement('h1');
+    title_el.innerText = this.data.title;
+    card.appendChild(title_el)
+
+    let date_el = document.createElement('h2')
+    date_el.innerText = this.data.date;
+    card.appendChild(date_el);
+
+    let media_el;
+    if (this.data.media_type === 'video'){
+        media_el = document.createElement('iframe');
+    }else{
+        media_el = document.createElement('img');
+    }
+
+    var res;
+
+    res_btns.forEach(function(rad){
+        if (rad.checked){
+            res = rad.value;
+        }
+    })
+
+    if (res === 'hd') {
+        media_el.setAttribute('src', this.data.hdurl);
+        console.log('hfkhhguiuhgfuihgcfghuijhv', res)
+    } else {
+        media_el.setAttribute('src', this.data.url);
+    }
+
+    let media_cont = document.createElement('div');
+    media_cont.classList.add('modal')
+
+    media_cont.appendChild(media_el);
+    card.appendChild(media_cont);
+
+    let explain_el = document.createElement('p');
+    explain_el.innerText = this.data.explanation;
+    card.appendChild(explain_el);
+
+    container.prepend(card);
+    }
+}
--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const container = document.getElementById('container');
-const btn = document.getElementById('go');
+const date_btn = document.getElementById('go');
 const calandar = document.getElementById('date');
 const min_date = new Date('1995, 06, 16');
 const today_btn = document.getElementById('today_btn');
 
 console.log(document.getElementsByName('res'))
 const res_btns = document.getElementsByName('res')
-var res;
 
-res_btns.forEach(function(rad){
-    if (rad.checked){
-        res = rad.value;
-    }
-})
+var days = [];
 
-console.log("AAAAAAAA",res)
 console.log(min_date);
 
-randomDays().catch(err => {
+getData(10).catch(err => {
     console.log(err)
 })
 
 
-btn.addEventListener('click', function(){
+date_btn.addEventListener('click', function(){
     let date_val = calandar.value;
     getData(date_val).catch(err =>{
         console.log(err)
@@ -48,69 +42,33 @@ today_btn.addEventListener('click', function(){
     getData(`${year}-${month}-${day}`)
 })
 
-// getData().catch(err =>{
-//     console.log(err)
-// });
+async function getData(arg){
 
-async function getData(day){
+    var api_link
 
-    let date_obj = new Date(day);
-    if (date_obj < min_date){
-        return;
+    if (typeof arg === 'string'){
+        api_link = `https://api.nasa.gov/planetary/apod?api_key=${config.api_key}&date=${arg}`;
+    } else {
+        api_link = `https://api.nasa.gov/planetary/apod?api_key=${config.api_key}&count=${arg}`;
+
     }
-
-    let count = 10;
-    const api_link = `https://api.nasa.gov/planetary/apod?api_key=${config.api_key}&date=${day}`;
-    const response = await fetch(api_link)
-    const data = await response.json();
+    
+    var response = await fetch(api_link)
+    var data = await response.json();
     console.log(data);
 
-    // data.forEach(function(day){
-    //     createView(day)
-    // })
-
-    createView(data);
-}
-
-async function randomDays(){
-    let count = 10;
-    const api_link = `https://api.nasa.gov/planetary/apod?api_key=${config.api_key}&count=${count}`;
-    const response = await fetch(api_link)
-    const data = await response.json();
-    console.log(data);
+    if (!(Array.isArray(data))){
+        data = [data];
+    }
 
     data.forEach(function(day){
-         createView(day)
+        //  createView(day)
+        days.push(new Day(day));
      })
 
-}
+     console.log(days);
+     days.forEach(function(day){
+         day.createView(container, res_btns)
+     })
 
-
-function createView(info){
-
-    let card = document.createElement('div');
-
-    let title_el = document.createElement('h1');
-    title_el.innerText = info.title;
-    card.appendChild(title_el)
-
-    let date_el = document.createElement('h2')
-    date_el.innerText = info.date;
-    card.appendChild(date_el);
-
-    let media_el;
-    if (info.media_type === 'video'){
-        media_el = document.createElement('iframe');
-    }else{
-        media_el = document.createElement('img');
-    }
-
-    media_el.setAttribute('src', info.url);
-    card.appendChild(media_el);
-
-    let explain_el = document.createElement('p');
-    explain_el.innerText = info.explanation;
-    card.appendChild(explain_el);
-
-    container.prepend(card);
 }
